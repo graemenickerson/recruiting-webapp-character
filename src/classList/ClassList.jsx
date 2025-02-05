a6ad90b2abccebc5cf5ce301e0abec9f8dfaac75
Graeme Nickerson <graeme.nickerson@gmail.com>
2025-02-05 14:08:00 -0800

    Add start of color changes to class list

--- src/classList/ClassList.jsx
+++ src/classList/ClassList.jsx
@@ -3,14 +3,19 @@ import { CLASS_LIST } from "../consts";
 const ClassList = ({character}) => {
   const listOfClasses = Object.keys(CLASS_LIST);
 
-  
+  const getClassColour = (item) => {
+    // check ability scores of character and change colour when all attributes meets or beats the CLASS_LIST score
+    
+    return "#ffffff"
+  }
 
   return (
     <div className="Class-List">
       <h3>Classes</h3>
       {listOfClasses.map((item) => {
+        const textColour = getClassColour(item)
         return(
-          <p>{item}</p>
+          <p style={{color: textColour}} key={item}>{item}</p>
         );
       })}
     </div>
